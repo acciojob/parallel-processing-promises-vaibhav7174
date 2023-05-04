9647db226b17d96dceccd80cf8ebe3827aeeeb8b
Vaibhav Humbade <117819312+vaibhav7174@users.noreply.github.com>
2023-05-04 12:32:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+function downloadImages() {
+	const promises=images.map(url =>{
+		return  new Promise((resolve,reject)=>{
+			const img=new Images();
+			img.onload=() =>resolve(img);
+			img.onerror=()=>reject('Failed to load image's URL: ${image.url}');
+		    img.src=url;
+		});
+	});
+	promise.all(promises)
+	.then(images=>{
+		const outputDiv=document.getElementById('output');
+		images.forEach(image=>{
+			outputDiv.appendChild(image);
+		});
+	})
+	.catch(error=>{
+		console.log(error);
+	});
+}
+document.getElementById('download-images-button').
+	addEventListener('click',downloadImages);
